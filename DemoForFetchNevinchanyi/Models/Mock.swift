3199a2b4fa61f4d156f0a0a1c2b4fb06ac29b5e2
Kostiantyn Nevinchanyi <konstantin.nevinchanyi@bambuser.com>
2023-04-05 11:07:20 -0400

    Add Dessert and Mock models.

---
+++ DemoForFetchNevinchanyi/Models/Mock.swift
@@ -0,0 +1,15 @@
+//
+//  Mock.swift
+//  DemoForFetchNevinchanyi
+//
+//  Created by Kostiantyn Nevinchanyi on 4/5/23.
+//
+
+import Foundation
+
+
+struct Mock {
+    static let dessert = Dessert(id: "53049",
+                                 name: "Apam balik",
+                                 thumbnail: "https://www.themealdb.com/images/media/meals/adxcbq1619787919.jpg")
+}
