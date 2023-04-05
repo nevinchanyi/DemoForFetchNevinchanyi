33a1b5da2f7eed0672a6e3c70d079147b5f0154f
Kostiantyn Nevinchanyi <konstantin.nevinchanyi@bambuser.com>
2023-04-05 10:45:03 -0400

    Initial Commit

---
+++ DemoForFetchNevinchanyi/DemoForFetchNevinchanyiApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemoForFetchNevinchanyiApp.swift
+//  DemoForFetchNevinchanyi
+//
+//  Created by Kostiantyn Nevinchanyi on 4/5/23.
+//
+
+import SwiftUI
+
+@main
+struct DemoForFetchNevinchanyiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
